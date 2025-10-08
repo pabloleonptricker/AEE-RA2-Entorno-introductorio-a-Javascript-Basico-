4410b121231b11cdbd4f94450622c21ec78edda1
Pablo.Leon <pabloleon.24@campuscamara.es>
2025-10-08 09:12:50 +0200

    ej1 completado

---
+++ Ejercicio1.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 5;
+let suma = a + b;
+let resta = a - b;
+let multiplicacion = a * b;
+let division = a / b;
+console.log("Resultados: Suma: " + suma + ", resta: " + resta + ", multiplicación: " + multiplicacion + ", división: " + division);

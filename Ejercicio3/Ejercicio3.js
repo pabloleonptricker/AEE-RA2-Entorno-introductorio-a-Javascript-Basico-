e20af5442c42d7b3424a5a3868229bfaf205c52c
Pablo.Leon <pabloleon.24@campuscamara.es>
2025-10-08 09:42:55 +0200

    arreglos y fin ejercicio 3

---
+++ Ejercicio3/Ejercicio3.js
@@ -0,0 +1,23 @@
+function sumar(num1, num2){
+    suma = num1 + num2;
+    console.log(suma);
+}
+
+function restar(num1, num2){
+    resta = num1 - num2;
+    console.log(resta);
+}
+
+function multiplicar(num1, num2){
+    multiplicacion = num1 * num2;
+    console.log(multiplicacion);
+}
+
+function dividir(num1, num2){
+    if(num2 == 0){
+        console.log("No se puede dividir entre 0")}else{
+        division = num1 / num2;
+        console.log(division);
+        }
+    
+}
